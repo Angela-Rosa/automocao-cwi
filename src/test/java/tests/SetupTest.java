5dd8fb99a15cbfc2a68a10dd18e02ba44c0e16d7
Angela-rosa <angela.ards@gmail.com>
2021-10-10 19:30:20 -0300

    adcionando ao teste de login do video 1 - cenario de teste

--- src/test/java/tests/SetupTest.java
+++ src/test/java/tests/SetupTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import utils.Browser;
 import utils.Utils;
 
@@ -15,5 +16,22 @@ public class SetupTest extends BaseTests{
         System.out.println("Abrimos o navegador e carregamos a url do site de teste!");
 
     }
+@Test
+    public void testLogin(){
+        Browser.getCurrentDriver().findElement(By.className("login")).click();
+        System.out.println("Clicou em  Sing in e redirecionou");
+        assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getBaseUrl().concat("index.php?controller=authentication&back=my-account")));
+        Browser.getCurrentDriver().findElement(By.id("email")).sendKeys("rrsetcwi+autopractice@gmail.com");
+    System.out.println(" Preencheu o email");
+        Browser.getCurrentDriver().findElement(By.id("passwd")).sendKeys("teste123");
+    System.out.println("Preencheu a senha");
+        Browser.getCurrentDriver().findElement(By.id("SubmitLogin")).click();
+    System.out.println("Clicou em  Sing in");
+    assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getBaseUrl().concat("index.php?controller=my-account")));
+    System.out.println(" Validou a url do login");
+    assertTrue(Browser.getCurrentDriver().findElement(By.className("page-heading")).getText().contains("MY ACCOUNT"));
+    System.out.println(" Validou titulo da pagina");
+
+}
 
 }
